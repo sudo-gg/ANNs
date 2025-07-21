a2708b0517ee70899c3223a7d96c04b3c1119111
J S <joshuajcsmith@icloud.com>
2025-07-21 17:54:07 +0100

    Added a README for master

--- DataLoader.py
+++ DataLoader.py
@@ -1,9 +1,10 @@
 import gzip
 import pickle
 import numpy as np
+# pickle converts python objects to bytes and gzip compresses them
 
 def loadData():
-    f = gzip.open('/data/mnist.pkl.gz','rb')
+    f = gzip.open('data/mnist.pkl.gz','rb')
     trainingData, validationData, testData = pickle.load(f)
     f.close()
     return trainingData, validationData, testData
